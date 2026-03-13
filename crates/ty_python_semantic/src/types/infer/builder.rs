5715fb57aa449eb99c0c3b569cf42309ca86a040
Charlie Marsh <charlie.r.marsh@gmail.com>
2026-03-13 14:38:42 -0400

    [ty] Narrow TypedDicts through isinstance(..., dict)

--- crates/ty_python_semantic/src/types/infer/builder.rs
+++ crates/ty_python_semantic/src/types/infer/builder.rs
@@ -2331,6 +2331,7 @@ impl<'db, 'ast> TypeInferenceBuilder<'db, 'ast> {
             | Type::AlwaysFalsy
             | Type::TypeIs(_)
             | Type::TypeGuard(_)
+            | Type::TypedDictTop
             | Type::TypedDict(_)
             | Type::NewTypeInstance(_) => {
                 // We may infer the value type multiple times with distinct type context during
@@ -2935,6 +2936,7 @@ impl<'db, 'ast> TypeInferenceBuilder<'db, 'ast> {
             | Type::TypeIs(_)
             | Type::TypeGuard(_)
             | Type::TypedDict(_)
+            | Type::TypedDictTop
             | Type::NewTypeInstance(_) => {
                 let delattr_dunder_call_result = object_ty.try_call_dunder_with_policy(
                     db,
@@ -3109,7 +3111,8 @@ impl<'db, 'ast> TypeInferenceBuilder<'db, 'ast> {
                 | Type::Divergent(_)
                 | Type::Never
                 | Type::ModuleLiteral(..)
-                | Type::BoundSuper(..) => return None,
+                | Type::BoundSuper(..)
+                | Type::TypedDictTop => return None,
             })
         } else {
             None
@@ -4756,6 +4759,7 @@ impl<'db, 'ast> TypeInferenceBuilder<'db, 'ast> {
                 | Type::BoundSuper(_)
                 | Type::TypeIs(_)
                 | Type::TypeGuard(_)
+                | Type::TypedDictTop
                 | Type::TypedDict(_)
                 | Type::NewTypeInstance(_) => None,
             }
@@ -8764,6 +8768,7 @@ impl<'db, 'ast> TypeInferenceBuilder<'db, 'ast> {
                 | Type::BoundSuper(_)
                 | Type::TypeIs(_)
                 | Type::TypeGuard(_)
+                | Type::TypedDictTop
                 | Type::TypedDict(_)
                 | Type::NewTypeInstance(_),
             ) => fallback_unary_expression_type(),
