c34addb7f75c39f76b5b210c555f0940afa01264
Charlie Marsh <charlie.r.marsh@gmail.com>
2026-04-09 13:04:33 -0400

    Add a special form for TypedDictTop

--- crates/ty_python_semantic/src/types.rs
+++ crates/ty_python_semantic/src/types.rs
@@ -5274,7 +5274,6 @@ impl<'db> Type<'db> {
             | Type::PropertyInstance(_)
             | Type::TypeIs(_)
             | Type::TypeGuard(_)
-            | Type::TypedDictTop
             | Type::TypedDict(_) => Err(InvalidTypeExpressionError {
                 invalid_expressions: smallvec_inline![InvalidTypeExpression::InvalidType(
                     *self, scope_id
@@ -5412,7 +5411,7 @@ impl<'db> Type<'db> {
                 }
             }
 
-            Type::Dynamic(_) | Type::Divergent(_) => Ok(*self),
+            Type::TypedDictTop | Type::Dynamic(_) | Type::Divergent(_) => Ok(*self),
 
             Type::NominalInstance(instance) => match instance.known_class(db) {
                 Some(KnownClass::NoneType) => Ok(Type::none(db)),
@@ -6286,7 +6285,7 @@ impl<'db> Type<'db> {
                 Protocol::Synthesized(_) => None,
             },
 
-            Self::TypedDictTop => None,
+            Self::TypedDictTop => Type::SpecialForm(SpecialFormType::TypedDictTop).definition(db),
             Self::TypedDict(typed_dict) => typed_dict.type_definition(db),
 
             Self::Union(_) | Self::Intersection(_) => None,
--- crates/ty_python_semantic/src/types/infer/builder/type_expression.rs
+++ crates/ty_python_semantic/src/types/infer/builder/type_expression.rs
@@ -2185,6 +2185,7 @@ impl<'db> TypeInferenceBuilder<'db, '_> {
             SpecialFormType::TypingSelf
             | SpecialFormType::TypeAlias
             | SpecialFormType::TypedDict
+            | SpecialFormType::TypedDictTop
             | SpecialFormType::Unknown
             | SpecialFormType::Any
             | SpecialFormType::NamedTuple => {
